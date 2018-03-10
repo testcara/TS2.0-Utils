cf19e8de96dd7df54fb35b3ffcd54db0444cee35
Cara Wang <wlin@redhat.com>
2018-03-10 23:03:43 +0800

    Add the data base clean script for env related scripts

---
+++ env_related/test.js
@@ -0,0 +1,10 @@
+db
+use pulp_database
+db.units_erratum.drop()
+db.units_rpm.drop()
+db.units_drpm.drop()
+db.units_srpm.drop()
+db.task_status.drop()
+db.units_node.drop()
+db.units_iso.drop()
+db.repo_content_units.drop()
